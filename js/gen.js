ff397180c49f4cfe1c96d30fccfa499575e7ca6f
small <37457861+zmall53@users.noreply.github.com>
2018-03-20 21:30:55 +0000

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -1,7 +1,7 @@
 //Name Generator -Thanks to users on stack overflow-
 
-var a = ["Emerald", "Wet", "Old", "SWAG", "Black", "White", "Juicy", "Screaming", "Fluffy", "Annoying", "Oblivious", "Sticky", "Tired", "Awesome", "Funny", "Smart", "Brave", "Calm", "Jealous", "Blue", "Attractive", "Fancy", "Fit", "Long", "Handsome", "Beautiful", "Plump", "Purple", "Salty", "Red", "Green", "Famous", "Helpful", "Rich", "Small"];
-var b = ["shoe", "rabbit", "creeper", "block", "cereal", "animal", "fish", "tree", "mouse", "bottle", "bird", "brother", "gamer", "flower", "monkey", "ape", "spider", "baby"];
+var a = ["Emerald", "Wet", "Old", "SWAG", "Black", "White", "Juicy", "Screaming", "Fluffy", "Dumb", "Annoying", "Oblivious", "Sticky", "Tired", "Awesome", "Funny", "Smart", "Brave", "Calm", "Jealous", "Blue", "Attractive", "Fancy", "Fit", "Long", "Handsome", "Beautiful", "Plump", "Purple", "Salty", "Red", "Green", "Famous", "Helpful", "Rich", "Small", "Jolly", "Blonde", "Emo"];
+var b = ["shoe", "rabbit", "creeper", "block", "cereal", "animal", "fish", "tree", "mouse", "bottle", "bird", "brother", "gamer", "flower", "monkey", "ape", "spider", "star", "panther", "sister", "ginger", "dog", "grandma", "grandpa", "whale", "water", "chicken", "horse", "man", "rat", "book", "fan", "hand"];
 var name = "nameQuick";
 var hasCreate = 0;
 

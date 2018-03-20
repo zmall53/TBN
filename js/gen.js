b44c3a3dc665e2ac77b242576cdd3cf4c793611d
small <37457861+zmall53@users.noreply.github.com>
2018-03-20 05:44:56 +0000

    Changed ID from genname to namegen

--- js/gen.js
+++ js/gen.js
@@ -5,4 +5,4 @@
    var rA = Math.floor(Math.random()*a.length);
    var rB = Math.floor(Math.random()*b.length);
    var name = a[rA] + b[rB];
-   document.getElementById("genName").innerHTML = name;
+   document.getElementById("nameGen").innerHTML = name;

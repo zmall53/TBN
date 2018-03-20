fb77f3a894ac4aa3b02aa5ea3361371cd45c7550
small <37457861+zmall53@users.noreply.github.com>
2018-03-20 05:41:52 +0000

    Added JS

---
+++ js/gen.js
@@ -0,0 +1,8 @@
+   var a = ["Small", "Blue", "Ugly"];
+   var b = ["Bear", "Dog", "Banana"];
+
+
+   var rA = Math.floor(Math.random()*a.length);
+   var rB = Math.floor(Math.random()*b.length);
+   var name = a[rA] + b[rB];
+   document.getElementById("bigboi").innerHTML = name;

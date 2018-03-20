0fb5966b5b4d3fd2e5a4770aaaa64cfc0a0d7f81
small <37457861+zmall53@users.noreply.github.com>
2018-03-20 05:50:01 +0000

    Cleaned up code

--- js/gen.js
+++ js/gen.js
@@ -1,14 +1,14 @@
-   //Name Generator -Thanks to users on stack overflow-
-   var a = ["Small", "Blue", "Ugly"];
-   var b = ["Bear", "Dog", "Banana"];
+//Name Generator -Thanks to users on stack overflow-
+var a = ["Small", "Blue", "Ugly"];
+var b = ["Bear", "Dog", "Banana"];
 
 
-   var rA = Math.floor(Math.random()*a.length);
-   var rB = Math.floor(Math.random()*b.length);
-   var name = a[rA] + b[rB];
-   document.getElementById("nameGen").innerHTML = name;
+var rA = Math.floor(Math.random()*a.length);
+var rB = Math.floor(Math.random()*b.length);
+var name = a[rA] + b[rB];
+document.getElementById("nameGen").innerHTML = name;
 
-   //Reload
+//Reload
 function pageReload() {
     location.reload();
 }

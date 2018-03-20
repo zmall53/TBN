e438f70a4cea4b516c99032917f8e96cfeee4a09
small <37457861+zmall53@users.noreply.github.com>
2018-03-20 05:54:27 +0000

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -1,4 +1,5 @@
 //Name Generator -Thanks to users on stack overflow-
+function nameCreate() {
 var a = ["Small", "Blue", "Ugly"];
 var b = ["Bear", "Dog", "Banana"];
 
@@ -7,6 +8,7 @@ var rA = Math.floor(Math.random()*a.length);
 var rB = Math.floor(Math.random()*b.length);
 var name = a[rA] + b[rB];
 document.getElementById("nameGen").innerHTML = name;
+}
 
 //Reload
 function pageReload() {

a866cb79d88220e43dd281a10ef670ddb76f6f7b
zmall53 <37457861+zmall53@users.noreply.github.com>
2018-03-21 23:42:56 +0000

    Cleaned up JS

--- js/gen.js
+++ js/gen.js
@@ -6,21 +6,22 @@ var name = "nameQuick";
 var hasCreate = 0;
 
 function nameCreate() {
-    var rA = Math.floor(Math.random()*a.length);
-    var rB = Math.floor(Math.random()*b.length);
-    name = a[rA] + b[rB];
-    document.getElementById("nameGen").innerHTML = name;
-    hasCreate = 1;
+  var rA = Math.floor(Math.random() * a.length);
+  var rB = Math.floor(Math.random() * b.length);
+  name = a[rA] + b[rB];
+  document.getElementById("nameGen").innerHTML = name;
+  hasCreate = 1;
 }
 
 function addNumb() {
-    if (hasCreate == 1) {
-        var numb = Math.floor(Math.random() * 99);
-        var nameNumb = name + numb;
-        document.getElementById("nameGen").innerHTML = nameNumb;
-    } else {
-     document.getElementById("numbButton").classList.add('buttonRed');
-     setTimeout(function(){ document.getElementById("numbButton").classList.remove('buttonRed'); }, 400);
-    }
+  if (hasCreate == 1) {
+    var numb = Math.floor(Math.random() * 99);
+    var nameNumb = name + numb;
+    document.getElementById("nameGen").innerHTML = nameNumb;
+  } else {
+    document.getElementById("numbButton").classList.add('buttonRed');
+    setTimeout(function() {
+      document.getElementById("numbButton").classList.remove('buttonRed');
+    }, 400);
+  }
 }
-

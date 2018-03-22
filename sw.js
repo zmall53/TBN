e363a77e5259be735b1659eac09daaad232a7ad2
small <37457861+zmall53@users.noreply.github.com>
2018-03-22 00:38:59 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install'+ response.url);
+        console.log('[PWA] Cached index page during Install'+ response.url);
         return cache.put(indexPage, response);
       });
   }));
@@ -13,7 +13,7 @@ self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
     return caches.open('pwabuilder-offline').then(function (cache) {
       return fetch(request).then(function (response) {
-        console.log('[PWA Builder] add page to offline'+response.url)
+        console.log('[PWA] add page to offline'+response.url)
         return cache.put(request, response);
       });
     });
@@ -21,7 +21,7 @@ self.addEventListener('fetch', function(event) {
   event.waitUntil(updateCache(event.request));
   event.respondWith(
     fetch(event.request).catch(function(error) {
-      console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+      console.log( '[PWA] Network request Failed. Serving content from cache: ' + error );
       return caches.open('pwabuilder-offline').then(function (cache) {
         return cache.match(event.request).then(function (matching) {
           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;

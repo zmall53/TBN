161b5316773cfdcd398bfe1692008cc9a27649cb
small <37457861+zmall53@users.noreply.github.com>
2018-03-22 00:28:15 +0000

    Update gen.js

--- js/gen.js
+++ js/gen.js
@@ -1,6 +1,6 @@
 //Name Generator -Thanks to users on stack overflow-
 
-navigator.serviceWorker && navigator.serviceWorker.register('/sw.js').then(function(registration) {
+navigator.serviceWorker && navigator.serviceWorker.register('sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
 });
 
